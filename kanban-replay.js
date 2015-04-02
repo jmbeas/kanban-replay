f861216cf5f4969613ff9ed00b1fad8971379715
jmbeas <jose.m.beas@gmail.com>
2015-04-02 13:32:07 +0200

    Refactor. Fillboard extracted to a function

--- kanban-replay.js
+++ kanban-replay.js
@@ -1,13 +1,18 @@
+var fillBoard = function(theBoard,columns) {
+  for (i = 0; i < columns.length; i++) {
+    var column_name = columns[i];
+    var new_column = $("<div>").addClass("col").attr("id",column_name);
+    var column_title = $("<div>").addClass("col-title").text(column_name);
+    new_column.append(column_title)
+    theBoard.append(new_column);
+  }
+};
+
 (function (document, $, storage) {
   $(document).ready(function() {
+    var theBoard = $("#board");
     var columns = ["to-do","wip","done"];
-    for (i = 0; i < columns.length; i++) {
-      var column_name = columns[i];
-      var new_column = $("<div>").addClass("col").attr("id",column_name);
-      var column_title = $("<div>").addClass("col-title").text(column_name);
-      new_column.append(column_title)
-      $("#board").append(new_column);
-    }
+    fillBoard(theBoard,columns);
     var new_card = $("<div>").addClass("card");
     $("#to-do").append(new_card);
   });
