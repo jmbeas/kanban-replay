d37ffc9a9743a93913dc400ad60a775d4cb1f2ec
jmbeas <jose.m.beas@gmail.com>
2015-04-02 13:36:32 +0200

    Refactor. Object-oriented.

--- kanban-replay.js
+++ kanban-replay.js
@@ -1,18 +1,20 @@
-var fillBoard = function(theBoard,columns) {
-  for (i = 0; i < columns.length; i++) {
-    var column_name = columns[i];
-    var new_column = $("<div>").addClass("col").attr("id",column_name);
-    var column_title = $("<div>").addClass("col-title").text(column_name);
-    new_column.append(column_title)
-    theBoard.append(new_column);
-  }
-};
+function Board(element) {
+  this.theBoard = element;
+  this.fill = function(columns) {
+    for (i = 0; i < columns.length; i++) {
+      var column_name = columns[i];
+      var new_column = $("<div>").addClass("col").attr("id",column_name);
+      var column_title = $("<div>").addClass("col-title").text(column_name);
+      new_column.append(column_title)
+      this.theBoard.append(new_column);
+    }
+  };
+}
 
 (function (document, $, storage) {
   $(document).ready(function() {
-    var theBoard = $("#board");
-    var columns = ["to-do","wip","done"];
-    fillBoard(theBoard,columns);
+    var theBoard = new Board( $("#board") );
+    theBoard.fill( ["to-do","wip","done"] );
     var new_card = $("<div>").addClass("card");
     $("#to-do").append(new_card);
   });
