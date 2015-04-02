41bda445e7ba0677865d2a0020d69c5ca5b73f6f
jmbeas <jose.m.beas@gmail.com>
2015-04-02 13:23:38 +0200

    Each column has its own name

--- kanban-replay.js
+++ kanban-replay.js
@@ -2,7 +2,10 @@
   $(document).ready(function() {
     var columns = ["to-do","wip","done"];
     for (i = 0; i < columns.length; i++) {
-      var new_column = $("<div>").addClass("col").attr("id",columns[i]);
+      var column_name = columns[i];
+      var new_column = $("<div>").addClass("col").attr("id",column_name);
+      var column_title = $("<div>").addClass("col-title").text(column_name);
+      new_column.append(column_title)
       $("#board").append(new_column);
     }
     var new_card = $("<div>").addClass("card");
